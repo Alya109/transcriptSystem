669e59c5290dad9baf2d3b4fe8a63060235bbea5
Alya <149942623+Alya109@users.noreply.github.com>
2025-01-18 07:42:40 +0800

    Update transcript.py

--- transcript.py
+++ transcript.py
@@ -17,18 +17,21 @@ def startFeature():
     print("M - Master")
     print("D - Doctorate")
     print("B0 - Both")
-    degree = input("Degree level: ").upper()
+    degree = input("Degree level (M/D/B0): ").upper()
   
-  time.sleep(3)
-  menuFeature
+  time.sleep(2)
+  menuFeature()
   return level, degree
     
   # No degree feature as of now :(
 def menuFeature():
-  print("Student Transcript Generation System")
+  print("\033[1mStudent Transcript Generation System\033[0m")
   print("======================================")
-  print("")
-  print("")
+  print("1. Student Details\n2.Statistics\n3.Transcript based on major courses")
+  print("4. Transcript based on minor courses\n5. Full Transcript\n6. Previous transcript request")
+  print("7. Select another student\n8. Terminate the system")
+  print("======================================")
+  print("\033[1mEnter your feature: \033[0m")
 
 def whateverFeatureThisIs():
 
