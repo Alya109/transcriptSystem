41fe472fd85babe4146d31d27795f739785771fa
Alya <149942623+Alya109@users.noreply.github.com>
2025-01-17 09:16:56 +0800

    Update transcript.py

--- transcript.py
+++ transcript.py
@@ -1,16 +1,28 @@
 import time
 import numpy
 
+
+# Note: No global variables
+
+
+# Start feature asking for student level and degree
 def startFeature():
   print("Select Student Level:")
   print("U - Undergraduate")
   print("G - Graduate")
   print("B - Both")
   level = input("Enter your choice (U/G/B): ").upper()
-
-
-
+  # No degree feature as of now :(
 
 def menuFeature():
   palautog
+
+def whateverFeatureThisIs():
+
+def featureOne():
+
+def featureTwo():
+
+
+
 
