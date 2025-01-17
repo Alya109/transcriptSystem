b914242d091e567daaf3f9f963845ce02a88ee1e
Alya <149942623+Alya109@users.noreply.github.com>
2025-01-17 09:12:51 +0800

    Update transcript.py

--- transcript.py
+++ transcript.py
@@ -1,7 +1,12 @@
-
+import time
+import numpy
 
 def startFeature():
-  ulol
+  print("Select Student Level:")
+  print("U - Undergraduate")
+  print("G - Graduate")
+  print("B - Both")
+  level = input("Enter your choice (U/G/B): ").upper()
 
 
 
